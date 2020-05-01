792c60700004415690efa13e46983b331ce3a9db
Andrew Nebel <nebela@msoe.edu>
2020-05-01 00:29:08 -0500

    Implemented help menu

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,7 +1,10 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
@@ -13,11 +16,19 @@ import java.util.concurrent.TimeUnit;
 
 public class Controller {
 
+    @FXML
+    TextArea helpMenu;
+    @FXML
+    Button helpMenuButton;
+
+    @FXML
+    MenuButton ticketBooth;
 
     @FXML
     MenuItem ticketBoothFromApts;
     @FXML
     MenuItem ticketBoothFromRamp;
+
     @FXML
     MediaView video;
 
@@ -26,16 +37,45 @@ public class Controller {
 
 
     @FXML
-    public void playTicketBoothFromRamp(){
-
-            String path = new File("src/testVideos/Funny2secondvideo.mp4").getAbsolutePath();
+    private void playTicketBoothFromRamp(){
+            String path = new File("src/smokeVideos/mirage/CTARamp.mp4").getAbsolutePath();
             media = new Media(new File(path).toURI().toString());
             mediaPlayer = new MediaPlayer(media);
             mediaPlayer.setOnEndOfMedia(() -> {
                 video.setMediaPlayer(null);
+                showAllMenuButtons();
             });
             video.setMediaPlayer(mediaPlayer);
+            hideAllMenuButtons();
             mediaPlayer.play();
+    }
+
+    private void hideAllMenuButtons(){
+        ticketBooth.setVisible(false);
+        helpMenuButton.setVisible(false);
+    }
+
+    private void showAllMenuButtons(){
+        ticketBooth.setVisible(true);
+        helpMenuButton.setVisible(true);
+    }
+
+    @FXML
+    private void stopMediaPlayer(){
+        video.setMediaPlayer(null);
+        showAllMenuButtons();
+    }
+
+    @FXML
+    private void showHelpMenu(){
+        helpMenu.setVisible(true);
+        helpMenuButton.setVisible(false);
+    }
+
+    @FXML
+    private void hideHelpMenu(){
+        helpMenu.setVisible(false);
+        helpMenuButton.setVisible(true);
     }
 }
 
